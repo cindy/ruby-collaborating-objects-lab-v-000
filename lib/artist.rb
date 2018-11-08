1c552cbf671c434f63d15225f1e796e8fe3b57ed
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-11-08 19:51:47 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,12 +4,15 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    @@all << self
   end
 
   def add_song(song)
     @songs << song
     # song.artist = self
+  end
+
+  def save
+    @@all << self
   end
 
 end
