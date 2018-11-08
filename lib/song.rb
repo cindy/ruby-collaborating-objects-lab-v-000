bef33cce0cf1aea0ee6851f906419dac32074066
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-11-08 20:25:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,9 @@
 class Song
-  attr_accessor(:name)
-  def initialize(name)
-    @name = name
+  attr_accessor(:title, :name)
+  def initialize(title)
+    @title = title
   end
 
   def new_by_filename(filename)
     filename.split('-')[0]
-  end
 end
